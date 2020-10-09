ef8443af7415fefc271f5407d90115c36a2c42d4
Leo Velez <leonardo.velez@kaplan.edu>
2020-10-09 18:45:41 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GASHAPONITEMS } from './itemList.contants';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'deadhaltgaschapon';
+  goldAmount = 0;
+  gaschaResult = 0;
+  itemDescrip = '';
+
+  pullLever(isCursedGold: boolean): void {
+    if (isCursedGold) {
+      this.gaschaResult = this.getRandomNumber(1, 100);
+    } else {
+      this.gaschaResult = this.goldAmount + this.getRandomNumber(1, 6) > 100 ? 100 : this.goldAmount + this.getRandomNumber(1, 6);
+    }
+    this.itemDescrip = `assets/${GASHAPONITEMS[this.gaschaResult - 1].descrip}.png`;
+  }
+
+  getRandomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 }
